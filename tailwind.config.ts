4e85c118e494db8e978ba3a11ec510b5f635c7bf
Himanshu Menghani <himanshumenghani524@gmail.com>
2024-12-15 14:32:49 +0530

    Refactor global styles, update Tailwind configuration, and simplify Home component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,17 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-} satisfies Config;
+  darkMode: "class",
+  plugins: [nextui()],
+};
+export default config;
